ccd869b402ff10039fd6cc637ed0da9dcd3cc02d
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-04 16:31:26 +0300

    Add use case test project

---
+++ RefillTerminal.UseCase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RefillTerminal.UseCase
+{
+    class Program
+    {
+        static string Ask(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var startTime = DateTime.Now;
+
+            var firstName = Ask("First name: ");
+            var lastName = Ask("Last name: ");
+            var accountId = Ask("Account ID: ");
+            var amount = ulong.Parse(Ask("Amount: "));
+
+            Console.WriteLine("\nUse case: begin\n");
+
+            var user = new TerminalUser(firstName, lastName, amount, accountId); 
+            var terminal = new Terminal();
+            var transaction = terminal.CreateTransaction(user.Payment);
+            terminal.PerformTransaction(transaction);
+            terminal.PrintCheck(transaction);
+
+            Console.WriteLine("\nUse case: end");
+            Console.WriteLine(DateTime.Now - startTime);
+        }
+    }
+}
